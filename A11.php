87397cc8d03f3f47c12ad23f1146c63591f3d0bf
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-03-24 16:41:38 -0500

    cc

--- A11.php
+++ A11.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Answer 12</title>
+  <title>Answer 11</title>
 </head>
 <body>
   <div>
@@ -19,23 +19,29 @@ include_once 'connection.php';
 
 // Input data
 $name = $_POST['name'];
+$year1 = $_POST['year1'];
+$year2 = $_POST['year2'];
 
-$sql = "SELECT year, candidate, SUM(candidatevotes) AS S1 FROM ptelect WHERE candidate LIKE '$name%' OR candidate LIKE '%$name' OR candidate LIKE '%$name%' GROUP BY year";
+$sql = "SELECT * FROM ptelect WHERE candidate LIKE '$name%' OR candidate LIKE '%$name' OR candidate LIKE '%$name%' AND year BETWEEN $year1 AND $year2";
 $result = mysqli_query($con, $sql) or die('Error ' . mysqli_error($con));
 
 echo "<table border='1'>
 <tr>
-<th style='padding:15px;'>Year</th>
 <th style='padding:15px;'>Candidate Name</th>
-<th style='padding:15px;'>No. of Votes</th>
+<th style='padding:15px;'>Year</th>
+<th style='padding:15px;'>Party</th>
+th style='padding:15px;'>State</th>
+th style='padding:15px;'>Votes</th>
 </tr>";
 
 while ($row = mysqli_fetch_array($result))
 {
   echo "<tr>";
-  echo "<td style='padding:15px;'>" . $row['year'] . "</td>";
   echo "<td style='padding:15px;'>" . $row['candidate'] . "</td>";
-  echo "<td style='padding:15px;'>" . $row['S1'] . "</td>";
+  echo "<td style='padding:15px;'>" . $row['year'] . "</td>";
+  echo "<td style='padding:15px;'>" . $row['party_simplified'] . "</td>";
+  echo "<td style='padding:15px;'>" . $row['state'] . "</td>";
+  echo "<td style='padding:15px;'>" . $row['candidatevotes'] . "</td>";
   echo "</tr>";
 }
 echo "</table>";
