73d10a35e9755d9bceb1bf1f159498ef21fe0514
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-03-24 15:53:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
-//connect mysql database
-$hostname = "uta.cloud";
-$username = "vvs1620_vivekshetye";
-$password = "Shaleshwar12@";
-$database = "vvs1620_database";
-
-$con = mysqli_connect($hostname, $username, $password, $database);
-
-if($con)
-{
-    echo "Connection Success";
-}
-else
-{
-    echo "Connection failed";
-}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bonus Quiz 03</title>
+</head>
+<body>
+	<div>
+		<h2>Student ID: 1001821620<br>Name: Vivek Vishwanath Shetye</h2><br><br><br>
+	</div>
+
+	<div>
+		<a href="Q10.html">Q10 - Click here!</a><br><br>
+
+		<a href="Q11.html">Q11 - Click here!</a><br><br>
+
+		<a href="Q12.html">Q12 - Click here!</a><br><br>
+	</div>
+</body>
+</html>
