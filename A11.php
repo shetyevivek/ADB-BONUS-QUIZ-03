6f06f8647c1302e7ba9f6132bc93e784a7925246
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-03-24 17:06:33 -0500

    Update A11.php

--- A11.php
+++ A11.php
@@ -56,10 +56,6 @@ echo "Q11.b<br>";
 
 $row2 = mysqli_fetch_array($result2);
 echo "Total of the votes received = " . $row2['S1'];
-/*while ($row2 = mysqli_fetch_array($result2))
-{
-  echo "Total of the votes received = " . $row2['S1'];
-}*/
 
 echo "<br><br>";
 
