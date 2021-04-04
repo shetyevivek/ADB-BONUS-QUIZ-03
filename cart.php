82762532c06a4d55da6752085845eaf3d343089d
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-04 03:14:20 -0500

    cc

--- cart.php
+++ cart.php
@@ -2,28 +2,27 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/mystyle.css">
-  <title>Basket</title>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+	<title>Basket</title>
 </head>
 
 <body>
-  <main>
-    <h2>Shopping Cart</h2>
+	<main>
+		<h2>Shopping Cart</h2>
+		<div class="basket">
+			<div class="basket-labels">
+				<ul>
+					<li class="item item-heading">Item</li>
+					<li class="price">Price</li>
+					<li class="quantity">Quantity</li>
+					<li class="subtotal">Subtotal</li>
+				</ul>
+			</div>
 
-    <div class="basket">
-      <div class="basket-labels">
-        <ul>
-          <li class="item item-heading">Item</li>
-          <li class="price">Price</li>
-          <li class="quantity">Quantity</li>
-          <li class="subtotal">Subtotal</li>
-        </ul>
-      </div>
-
-      <?php
+	<?php
       include_once "connection.php";
 
       $sql = "SELECT * FROM cart";
@@ -55,164 +54,112 @@
         echo "</div>";
         echo "</div>";
       }
-      ?>
 
-      <!--<div class="basket-product">
-        <div class="item">
-          <div class="product-image">
-            <img src="http://placehold.it/120x166" alt="Placholder Image 2" class="product-frame">
-          </div>
-          <div class="product-details">
-            <h1><strong><span class="item-quantity">4</span> x Eliza J</strong> Lace Sleeve Cuff Dress</h1>
-          </div>
+     ?>
+
         </div>
-        <div class="price">26.00</div>
-        <div class="quantity">
-          <input type="number" value="4" min="1" class="quantity-field">
-        </div>
-        <div class="subtotal">104.00</div>
-        <div class="remove">
-          <button>Remove</button>
-        </div>
-      </div>
+		<aside>
+			<div class="summary">
+				<div class="summary-total-items"><span class="total-items"></span> Items in your Bag</div>
+				<div class="summary-subtotal">
+					<div class="subtotal-title">Subtotal</div>
+					<div class="subtotal-value final-value" id="basket-subtotal">130.00</div>
+				</div>
+				<div class="summary-total">
+					<div class="total-title">Total</div>
+					<div class="total-value final-value" id="basket-total">130.00</div>
+				</div>
+				<div class="summary-checkout">
+					<button class="checkout-cta">Go to Secure Checkout</button>
+				</div>
+			</div>
+		</aside>
+	</main>
+	
+	<!-- JAVASCRIPT - DO NOT TOUCH -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+	<script type="text/javascript">
+	/* Set values + misc */
+	var promoCode;
+	var promoPrice;
+	var fadeTime = 300;
+	/* Assign actions */
+	$('.quantity input').change(function() {
+		updateQuantity(this);
+	});
+	$('.remove button').click(function() {
+		removeItem(this);
+	});
+	$(document).ready(function() {
+		updateSumItems();
+	});
+	/* Recalculate cart */
+	function recalculateCart(onlyTotal) {
+		var subtotal = 0;
+		/* Sum up row totals */
+		$('.basket-product').each(function() {
+			subtotal += parseFloat($(this).children('.subtotal').text());
+		});
+		/* Calculate totals */
+		var total = subtotal;
+		/*If switch for update only total, update only total display*/
+		if(onlyTotal) {
+			/* Update total display */
+			$('.total-value').fadeOut(fadeTime, function() {
+				$('#basket-total').html(total.toFixed(2));
+				$('.total-value').fadeIn(fadeTime);
+			});
+		} else {
+			/* Update summary display. */
+			$('.final-value').fadeOut(fadeTime, function() {
+				$('#basket-subtotal').html(subtotal.toFixed(2));
+				$('#basket-total').html(total.toFixed(2));
+				if(total == 0) {
+					$('.checkout-cta').fadeOut(fadeTime);
+				} else {
+					$('.checkout-cta').fadeIn(fadeTime);
+				}
+				$('.final-value').fadeIn(fadeTime);
+			});
+		}
+	}
+	/* Update quantity */
+	function updateQuantity(quantityInput) {
+		/* Calculate line price */
+		var productRow = $(quantityInput).parent().parent();
+		var price = parseFloat(productRow.children('.price').text());
+		var quantity = $(quantityInput).val();
+		var linePrice = price * quantity;
+		/* Update line price display and recalc cart totals */
+		productRow.children('.subtotal').each(function() {
+			$(this).fadeOut(fadeTime, function() {
+				$(this).text(linePrice.toFixed(2));
+				recalculateCart();
+				$(this).fadeIn(fadeTime);
+			});
+		});
+		productRow.find('.item-quantity').text(quantity);
+		updateSumItems();
+	}
 
-      <div class="basket-product">
-        <div class="item">
-          <div class="product-image">
-            <img src="http://placehold.it/120x166" alt="Placholder Image 2" class="product-frame">
-          </div>
-          <div class="product-details">
-            <h1><strong><span class="item-quantity">1</span> x Whistles</strong> Amella Lace Midi Dress</h1>
-          </div>
-        </div>
-        <div class="price">26.00</div>
-        <div class="quantity">
-          <input type="number" value="1" min="1" class="quantity-field">
-        </div>
-        <div class="subtotal">26.00</div>
-        <div class="remove">
-          <button>Remove</button>
-        </div>
-      </div>-->
-    </div>
-
-    <aside>
-      <div class="summary">
-        <div class="summary-total-items"><span class="total-items"></span> Items in your Bag</div>
-        <div class="summary-subtotal">
-          <div class="subtotal-title">Subtotal</div>
-          <div class="subtotal-value final-value" id="basket-subtotal">130.00</div>
-        </div>
-
-        <div class="summary-total">
-          <div class="total-title">Total</div>
-          <div class="total-value final-value" id="basket-total">130.00</div>
-        </div>
-        <div class="summary-checkout">
-          <button class="checkout-cta">Go to Secure Checkout</button>
-        </div>
-      </div>
-    </aside>
-  </main>
-
-
-<!-- JAVASCRIPT - DO NOT TOUCH -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-
-<script type="text/javascript">
-  /* Set values + misc */
-var promoCode;
-var promoPrice;
-var fadeTime = 300;
-
-/* Assign actions */
-$('.quantity input').change(function() {
-  updateQuantity(this);
-});
-
-$('.remove button').click(function() {
-  removeItem(this);
-});
-
-$(document).ready(function() {
-  updateSumItems();
-});
-
-/* Recalculate cart */
-function recalculateCart(onlyTotal) {
-  var subtotal = 0;
-
-  /* Sum up row totals */
-  $('.basket-product').each(function() {
-    subtotal += parseFloat($(this).children('.subtotal').text());
-  });
-
-  /* Calculate totals */
-  var total = subtotal;
-
-  /*If switch for update only total, update only total display*/
-  if (onlyTotal) {
-    /* Update total display */
-    $('.total-value').fadeOut(fadeTime, function() {
-      $('#basket-total').html(total.toFixed(2));
-      $('.total-value').fadeIn(fadeTime);
-    });
-  } else {
-    /* Update summary display. */
-    $('.final-value').fadeOut(fadeTime, function() {
-      $('#basket-subtotal').html(subtotal.toFixed(2));
-      $('#basket-total').html(total.toFixed(2));
-      if (total == 0) {
-        $('.checkout-cta').fadeOut(fadeTime);
-      } else {
-        $('.checkout-cta').fadeIn(fadeTime);
-      }
-      $('.final-value').fadeIn(fadeTime);
-    });
-  }
-}
-
-/* Update quantity */
-function updateQuantity(quantityInput) {
-  /* Calculate line price */
-  var productRow = $(quantityInput).parent().parent();
-  var price = parseFloat(productRow.children('.price').text());
-  var quantity = $(quantityInput).val();
-  var linePrice = price * quantity;
-
-  /* Update line price display and recalc cart totals */
-  productRow.children('.subtotal').each(function() {
-    $(this).fadeOut(fadeTime, function() {
-      $(this).text(linePrice.toFixed(2));
-      recalculateCart();
-      $(this).fadeIn(fadeTime);
-    });
-  });
-
-  productRow.find('.item-quantity').text(quantity);
-  updateSumItems();
-}
-
-function updateSumItems() {
-  var sumItems = 0;
-  $('.quantity input').each(function() {
-    sumItems += parseInt($(this).val());
-  });
-  $('.total-items').text(sumItems);
-}
-
-/* Remove item from cart */
-function removeItem(removeButton) {
-  /* Remove row from DOM and recalc cart total */
-  var productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function() {
-    productRow.remove();
-    recalculateCart();
-    updateSumItems();
-  });
-}
-</script>
-
+	function updateSumItems() {
+		var sumItems = 0;
+		$('.quantity input').each(function() {
+			sumItems += parseInt($(this).val());
+		});
+		$('.total-items').text(sumItems);
+	}
+	/* Remove item from cart */
+	function removeItem(removeButton) {
+		/* Remove row from DOM and recalc cart total */
+		var productRow = $(removeButton).parent().parent();
+		productRow.slideUp(fadeTime, function() {
+			productRow.remove();
+			recalculateCart();
+			updateSumItems();
+		});
+	}
+	</script>
 </body>
 
 </html>
