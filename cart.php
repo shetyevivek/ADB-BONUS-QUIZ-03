32e5c16c3994ccde6ba2257c2f25608be79e368c
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-04 02:50:11 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/mystyle.css">
   <title>Basket</title>
 </head>
 
