ee20faf5023f3cc7c152829e878b033542bbbf62
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-08 11:27:17 -0500

    cc

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
 <?php
+
+if($_POST)
+{
+	$name = $_POST['name'];
+	$comment = $_POST['comment'];
+	$handle = fopen("Vivek.txt", "a");
+	fwrite($handle, "<b><i>" .$name. "</i></b>:<br>" .$comment. "<br><br>");
+	fclose($handle);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="mystyle.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<title>Grocery Shopping</title>
+	<title>Comments Test</title>
+	<meta charset="utf-8">
 </head>
-
 <body>
-	
-<div class="topnav">
-  <a class="active" href="index.php">Home</a>
-  <a href="vegetables.php">Vegetables</a>
-  <a href="fruits.php">Fruits</a>
-  <a href="condiments.php">Condiments</a>
-  <div class="search-container">
-    <form action="search.php" method="POST">
-      <input type="text" placeholder="Search.." name="search">
-      <button type="submit" name="Submit">Submit</button>
-    </form>
-  </div>
-  <a href="cart.php" style="float:right;"><i class="fa fa-shopping-cart" style="font-size:19.5px;color:white"></i></a>
-</div>
-
+	<h1>Post a comment</h1>
+	<form action="" method="POST">
+		Name : <br><input type="text" name="name"><br>
+		Comment: <br><textarea rows="10" cols="30" name="comment"></textarea><br>
+		<input type="submit" name="Post comment">
+	</form>
+	<hr>
+	<h1>Other comments:</h1><br>
+	<?php
+	$name = "Vivek";
+	include '$name.txt';
+	?>
 </body>
 </html>
