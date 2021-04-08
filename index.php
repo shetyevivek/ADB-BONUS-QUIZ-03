070c295c599a2e12822aabbf6ceaa5e06fd3606a
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-08 03:33:24 -0500

    cc

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
-
-   session_start();
-   $_SESSION['superhero'] = "batman";
-
-   $name = "Vivek Shetye";
-
-   echo "<a href='page2.php?name=$name'>Go to the other page</a>";
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+</body>
+</html>
