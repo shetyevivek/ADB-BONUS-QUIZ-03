2d91202282095ed8c3f441cac31bb2173b39b525
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-08 11:20:51 -0500

    Update details.php

--- details.php
+++ details.php
@@ -96,7 +96,7 @@ if(isset($_POST['comments']))
 
 <h1 style="margin-left:20px;">Post a comment</h1>
   <form action="" method="POST">
-    <span style="margin-left:20px;">Name : </span><br><input type="text" name="naam" style="margin-left:20px;"><br>
+    <span style="margin-left:20px;">Name: </span><br><input type="text" name="naam" style="margin-left:20px;"><br><br>
     <span style="margin-left:20px;">Comment: </span><br><textarea rows="10" cols="30" name="comment" style="margin-left:20px;"></textarea><br>
     <input type="submit" name="comments" value="Post Comment" style="margin-left:20px;">
   </form>
