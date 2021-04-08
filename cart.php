f8ad0dad80c1c4e63a98440215a0c0a36a35c169
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-08 08:21:03 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -4,11 +4,27 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="mystyle3.css">
+	<link rel="stylesheet" type="text/css" href="mystyle2.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Shopping Cart</title>
 </head>
 
-<body>
+<body style="background:none;">
+
+	<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="vegetables.php">Vegetables</a>
+  <a href="fruits.php">Fruits</a>
+  <a href="condiments.php">Condiments</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+  <a href="cart.php" style="float:right;"><i class="fa fa-shopping-cart" style="font-size:19.5px;color:white"></i></a>
+</div>
+
 	<main>
 		<h2>Shopping Cart</h2>
 		<div class="basket">
