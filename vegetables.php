e5f8224160b06edaed0275870571630b37884783
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-08 04:47:09 -0500

    cc

--- vegetables.php
+++ vegetables.php
@@ -8,11 +8,11 @@
 <link rel="stylesheet" type="text/css" href="mystyle.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<title>Grocery Shopping</title>
+<title>Vegetables Shopping</title>
 </head>
 
-<body>
-	
+<body style="background:none;">
+
 <div class="topnav">
   <a class="active" href="index.php">Home</a>
   <a href="vegetables.php">Vegetables</a>
