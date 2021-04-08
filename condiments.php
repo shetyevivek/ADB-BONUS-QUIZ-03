7f0f21455894b3909e2e154d44f201cf23fad3f0
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-08 10:01:49 -0500

    cc

--- condiments.php
+++ condiments.php
@@ -1,6 +1,3 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,13 +16,41 @@
   <a href="fruits.php">Fruits</a>
   <a href="condiments.php">Condiments</a>
   <div class="search-container">
-    <form action="/action_page.php">
+    <form action="search.php" method="POST">
       <input type="text" placeholder="Search.." name="search">
       <button type="submit">Submit</button>
     </form>
   </div>
   <a href="cart.php" style="float:right;"><i class="fa fa-shopping-cart" style="font-size:19.5px;color:white"></i></a>
-</div>
+</div><br><br>
+
+<!-- Cards -->
+<?php
+include_once "connection.php";
+
+// Retrieve the data
+$sql = "SELECT * FROM grocery WHERE Category = 'condiments'";
+$result = mysqli_query($con, $sql) or die('Error ' . mysqli_error($con));
+
+echo "<div class='cards'>";
+
+while ($row = mysqli_fetch_array($result))
+{
+  $name = $row['Name'];
+  $price = $row['Price'];
+  $plocation = $row['Photo_Location'];
+
+  echo "<article class='card'>";
+  echo "<img src='$plocation'>";
+  echo "<div class='text' style='text-align:center;'>";
+  echo "<h1><a href='details.php?name=$name'>$name</a></h1>";
+  echo "<p class='price'>$$price.00</p>";
+  echo "</div></article>";
+}
+
+echo "</div>"
+
+?>
 
 </body>
 </html>
--- vegetables.php
+++ vegetables.php
@@ -16,7 +16,7 @@
   <a href="fruits.php">Fruits</a>
   <a href="condiments.php">Condiments</a>
   <div class="search-container">
-    <form action="/action_page.php">
+    <form action="search.php" method="POST">
       <input type="text" placeholder="Search.." name="search">
       <button type="submit">Submit</button>
     </form>
