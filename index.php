9b1eeaac9a72edccfe31cfd091465a9e76da8449
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-04-08 23:35:07 -0500

    Update

--- index.php
+++ index.php
@@ -4,28 +4,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="mystyle.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<title>Grocery Shopping</title>
+	<title>Bonus Quiz 03</title>
 </head>
-
 <body>
-	
-<div class="topnav">
-  <a class="active" href="index.php">Home</a>
-  <a href="vegetables.php">Vegetables</a>
-  <a href="fruits.php">Fruits</a>
-  <a href="condiments.php">Condiments</a>
-  <div class="search-container">
-    <form action="search.php" method="POST">
-      <input type="text" placeholder="Search.." name="search">
-      <button type="submit" name="Submit">Submit</button>
-    </form>
-  </div>
-  <a href="cart.php" style="float:right;"><i class="fa fa-shopping-cart" style="font-size:19.5px;color:white"></i></a>
-</div>
+	<div>
+		<h2>Student ID: 1001821620<br>Name: Vivek Vishwanath Shetye</h2><br><br><br>
+	</div>
 
+	<div>
+		<a href="#">Q10 - Look at the portion above.</a><br><br>
+
+		<a href="Q11.html">Q11a and Q11b - Click here!</a><br><br>
+	</div>
 </body>
 </html>
